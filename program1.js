9e408aef683c8ad14c674973f5274fd303b89846
unknown <abhideep.2004.3@gmail.com>
2024-11-11 13:42:20 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,6 +2,34 @@ const getTotalIsles = function (grid) {
 
 
   // write your code here
+  const rows = grid.length;
+  const cols = grid[0].length;
+  let count = 0;
+
+  const visited = new Array(rows).fill(false).map(() => new Array(cols).fill(false));
+
+  const dfs = (i, j) => {
+    if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] === 'W' || visited[i][j]) {
+      return;
+    }
+
+    visited[i][j] = true;
+    dfs(i + 1, j);
+    dfs(i - 1, j);
+    dfs(i, j + 1);
+    dfs(i, j - 1);
+  };
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === 'L' && !visited[i][j]) {
+        count++;
+        dfs(i, j);
+      }
+    }
+  }
+
+  return count;
 
 };
 
